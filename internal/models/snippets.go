25a7c11fe79061cdb62bacb25a8fa6ef126c7005
Marek Rubczewski <m.rubczewski@gmail.com>
2024-05-31 23:51:16 +0200

    Add all db methods

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -2,9 +2,10 @@ package models
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"fmt"
 	"github.com/jackc/pgx/v5/pgxpool"
-	"log"
 	"time"
 )
 
@@ -21,20 +22,57 @@ type SnippetModel struct {
 }
 
 func (m *SnippetModel) Insert(title string, content string, expiresAt time.Time) (int, error) {
-	_, err := m.DB.Exec(context.Background(), "INSERT INTO snippets (title, content, \"createdAt\", \"expiresAt\") VALUES ($1, $2, now(), $3)", title, content, expiresAt)
+	var id int
+
+	statement := "INSERT INTO snippets (title, content, \"createdAt\", \"expiresAt\") VALUES ($1, $2, now(), $3)"
+	err := m.DB.QueryRow(context.Background(), statement, title, content, expiresAt).Scan(&id)
 	if err != nil {
-		log.Fatalf("Insert failed: %v\n", err)
+		return 0, fmt.Errorf("insert failed: %w", err)
 	}
-	fmt.Println("Insert successful!")
-	return 0, nil
+
+	return id, nil
 }
 
-// This will return a specific snippet based on its id.
 func (m *SnippetModel) Get(id int) (Snippet, error) {
-	return Snippet{}, nil
+	statement := "SELECT id, title, content, \"createdAt\", \"expiresAt\" FROM snippets WHERE expires > NOW() AND id = ?"
+	row := m.DB.QueryRow(context.Background(), statement, id)
+	var s Snippet
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.CreatedAt, &s.ExpiresAt)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return Snippet{}, ErrNoRecord
+		} else {
+			return Snippet{}, err
+		}
+	}
+
+	return s, nil
 }
 
-// This will return the 10 most recently created snippets.
 func (m *SnippetModel) Latest() ([]Snippet, error) {
-	return nil, nil
+	statement := "SELECT id, title, content, \"createdAt\", \"expiresAt\" FROM snippets WHERE expires > NOW() ORDER BY \"createdAt\" DESC LIMIT 10"
+	rows, err := m.DB.Query(context.Background(), statement)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var snippets []Snippet
+	for rows.Next() {
+		var s Snippet
+
+		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.CreatedAt, &s.ExpiresAt)
+		if err != nil {
+			return nil, err
+		}
+		snippets = append(snippets, s)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return snippets, nil
 }
